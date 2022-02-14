305a85d37a15ee2bb712d4dd65a2be9b38d3c84b
Ailen Ariosto <ailen-1505@hotmail.com>
2022-02-14 17:36:09 -0300

    html5 semantico - clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,17 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
-var image = require('gulp-image');
-
-gulp.task('sass', () => {
-
-    return gulp.src('*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/assets/css'));
-        
-});
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
 
 gulp.task('pug', () => {
-
-    return gulp.src('./*.pug')
+    return gulp.src('src/views/*.pug')
         .pipe(pug({
             pretty: true
         }))
-        .pipe(gulp.dest('dist/'))
-                
+        .pipe(gulp.dest('dist/'))          
 });
 
-gulp.task('image', function () {
-    gulp.src('./assets/img/*')
-        .pipe(image())
-        .pipe(gulp.dest('./dist/assets/img/'));
+gulp.task('sass', () => {
+    return gulp.src('src/assets/scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/assets/css'));    
 });
-
-gulp.task('watch', () =>{
-
-    gulp.watch(['*.scss'], function(sass) {
-        sass();
-    });
-
-    gulp.watch(['*.pug'], function(pug) {
-        pug();
-    });
-
-});
-
-
